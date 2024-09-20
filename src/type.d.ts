a7afb5a47f2ccd6955085427dd0badb3abd17672
Harjot Saini <harjotsaini@192.168.1.3>
2024-09-21 01:21:06 +0530

    Added Base

---
+++ src/type.d.ts
@@ -0,0 +1,18 @@
+interface ITodo {
+  _id: string;
+  name: string;
+  description: string;
+  status: boolean;
+  createdAt?: string;
+  updatedAt?: string;
+}
+interface TodoProps {
+  todo: ITodo;
+}
+
+type ApiDataType = {
+  message: string;
+  status: string;
+  todos: ITodo[];
+  todo?: ITodo;
+};
